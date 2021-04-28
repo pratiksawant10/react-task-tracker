3126e9deb1cae92269d0beda1b08e86d747f1d78
Pratik <pratiksawant13@gmail.com>
2021-04-28 16:08:43 +1000

    feat: components and props

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import Button from './Button'
+
+const Header = ({ title }) => {
+    const onClick = () => {
+        alert('Click')
+    }
+
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
+        </header>
+    )
+} 
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+export default Header
