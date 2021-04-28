22e59332531a6456e5b4d29434b0b5fc07016e9e
Pratik <pratiksawant13@gmail.com>
2021-04-28 16:25:09 +1000

    feat: delete task and show msg when nothing

--- src/App.js
+++ src/App.js
@@ -7,19 +7,33 @@ const App = () => {
     {
         id: 1,
         text: "Docter Appointment",
-        args: ["Hello World"]
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
     },
     {
         id: 2,
-        text: "Grocery",
-        args: ["Good Bye"]
-    }
+        text: "Movies",
+        day: 'March 8th at 8:30pm',
+        reminder: false
+    },
+    {
+      id: 3,
+      text: "Grocery",
+      day: 'April 1st at 10:30am',
+      reminder: false
+  }
   ])
+
+  // Delete task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks To Show 2'}
     </div>
   );
 }
