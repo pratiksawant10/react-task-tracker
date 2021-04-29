6cc1b7ecae8fe8c67c735dccda6f7d033f7b4061
Pratik <pratiksawant13@gmail.com>
2021-04-29 14:23:31 +1000

    feat: form submit & toggle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,6 +26,15 @@ const App = () => {
     },
   ])
 
+  // Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete task
 
   const deleteTask = (id) => {
@@ -38,7 +49,8 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,16 @@
 import Button from './Button'
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        alert('Click')
-    }
-
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick} />
-        </header>
-    )
-} 
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+    </header>
+  )
+}
 
 Header.defaultProps = {
-    title: 'Task Tracker'
+  title: 'Task Tracker',
 }
 
 export default Header
